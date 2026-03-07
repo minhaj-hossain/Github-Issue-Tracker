d43e59636c0fb3cfe0b5997c8f67fd06c90242d0
Minhaj-Hossain <minhaj1080@gmail.com>
2026-03-07 19:43:18 +0600

    added login functionality

--- app/script.js
+++ app/script.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+// get the input field value from the login page
+const userName = document.getElementById("user-name");
+const password = document.getElementById("password");
+
+// login function
+function loadPage() {
+    
+    if(userName.value === "admin" && password.value === "admin123") {
+        window.location.href = "index.html";
+    }else {
+        return;
+    }
+
+}
+
+
+
+
+
+
